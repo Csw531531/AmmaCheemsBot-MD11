268944bdcb10c7116196adac8330fb9cb97e66d8
Csw531531 <senuralakshan456@gmail.com>
2025-03-04 03:58:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-//base by DGXeon
+i//base by DGXeon
 //re-upload? recode? copy code? give credit ya :)
 //YouTube: @DGXeon
 //Instagram: unicorn_xeon13
@@ -11,7 +11,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //session
-global.sessionid ='ur session id'
+global.sessionid ='03AA2E03D9672ABEF69E64CE2D15D87C'
 
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
@@ -20,10 +20,10 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
+global.ownernumber = '94769714218' //ur owner number
 global.ownername = '🦄드림 가이 Xeon' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.wagc = "https://whatsapp.com/channel/0029Vb1V3CF3AzNYoIIb9p2F"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
@@ -31,7 +31,7 @@ global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["94769714218"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -41,7 +41,7 @@ global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
